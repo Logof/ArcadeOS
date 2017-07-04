41a6d0415efe0da7021e970eba76426c6f35a761
Logof <Logof2008@mail.ru>
2017-07-04 04:48:51 -0400

    create file "createfs.sh"

---
+++ createfs.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+if [ ! ${1} ]
+then
+DIR=$(pwd)
+else
+DIR=${1}
+fi
+
+cd $DIR
+rm -Rf ./*
+mkdir -p dev etc root home proc media mnt sys tmp var
+mkdir -p usr/lib usr/local usr/games usr/share var/cache var/lib var/lock var/log var/games var/run var/spool media/cdrom media/flash media/usbdisk
+chmod 1777 tmp
